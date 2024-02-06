43e4a92a6f48fe60bc15e3c6cb5a1ad05622c826
Prashantkumar Singh <43060569+Prashantsingh2508@users.noreply.github.com>
2024-02-06 23:37:18 +0000

    Time: 0 ms (100.00%) | Memory: 44.9 MB (8.11%) - LeetSync

--- Problems/1580-shuffle-the-array/shuffle-the-array.java
+++ Problems/1580-shuffle-the-array/shuffle-the-array.java
@@ -1,14 +1,13 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] ans = new int[nums.length];
-        int index = 0;
+        int index=0;
+        int i=0;
+        while(i<n){
+            ans[index++]=nums[i];
+            ans[index++]=nums[n+i];
 
-        // Iterate over the first half of nums array
-        for (int i = 0; i < n; i++) {
-          
-            ans[index++] = nums[i];
-         
-            ans[index++] = nums[i + n];
+            i++;
         }
 
         return ans;
