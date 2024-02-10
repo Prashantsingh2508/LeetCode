bf648282def36817270913602b5e865a5bc2c06e
Prashantkumar Singh <43060569+Prashantsingh2508@users.noreply.github.com>
2024-02-10 19:00:08 +0000

    Time: 4 ms (89.90%) | Memory: 45.2 MB (48.14%) - LeetSync

--- Problems/557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ Problems/557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,24 +1,55 @@
+// class Solution {
+//     public String reverseWords(String s) {
+//         StringBuilder sb = new StringBuilder();
+//         StringBuilder word = new StringBuilder();
+
+//        for(char c:s.toCharArray()){
+
+
+//            if(c!=' '){
+
+//                word.append(c);
+
+//            }
+//            else{
+
+//                sb.append(word.reverse()).append(' ');
+//                 word.setLength(0);
+//            }
+
+//        }
+//        sb.append(word.reverse());
+//         return sb.toString();
+//     }
+// }
+
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder sb = new StringBuilder();
-        StringBuilder word = new StringBuilder();
+       int ls=-1;
+       char str[]=s.toCharArray();
+       for(int i=0;i<=s.length();i++){
 
-       for(char c:s.toCharArray()){
+            if(i==s.length() || str[i]==' '){
 
+                    int startIndex=ls+1;
+                    int endIndex=i-1;
+                    while(startIndex<endIndex){
 
-           if(c!=' '){
+                        char temp=str[startIndex];
+                        str[startIndex]=str[endIndex];
+                        str[endIndex]=temp;
+                        startIndex++;
+                        endIndex--;
 
-               word.append(c);
+                    }
 
-           }
-           else{
+                    ls=i;
 
-               sb.append(word.reverse()).append(' ');
-                word.setLength(0);
-           }
+            }
 
        }
-       sb.append(word.reverse());
-        return sb.toString();
+
+    return new String(str);
     }
 }
+
