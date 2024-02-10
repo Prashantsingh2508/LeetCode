b9723bbe1968a5235027c3a86b307b671f779880
Prashantkumar Singh <43060569+Prashantsingh2508@users.noreply.github.com>
2024-02-10 18:44:01 +0000

    Time: 7 ms (58.48%) | Memory: 45 MB (69.33%) - LeetSync

--- Problems/557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ Problems/557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -3,16 +3,22 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         StringBuilder word = new StringBuilder();
 
-        for (char c : s.toCharArray()) {
-            if (c != ' ') {
-                word.append(c);
-            } else {
-                sb.append(word.reverse()).append(' ');
-                word.setLength(0); // Clear the word StringBuilder
-            }
-        }
+       for(char c:s.toCharArray()){
 
-        sb.append(word.reverse()); // Append the last word
+
+           if(c!=' '){
+
+               word.append(c);
+
+           }
+           else{
+
+               sb.append(word.reverse()).append(' ');
+                word.setLength(0);
+           }
+
+       }
+       sb.append(word.reverse());
         return sb.toString();
     }
 }
