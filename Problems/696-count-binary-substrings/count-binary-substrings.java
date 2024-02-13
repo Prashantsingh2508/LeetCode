7b73e88d79d42778efc179d3aa9c93d6e3350707
Prashantkumar Singh <43060569+Prashantsingh2508@users.noreply.github.com>
2024-02-13 17:26:57 +0000

    Time: 13 ms (20.33%) | Memory: 45.1 MB (59.65%) - LeetSync

--- Problems/696-count-binary-substrings/count-binary-substrings.java
+++ Problems/696-count-binary-substrings/count-binary-substrings.java
@@ -1,22 +1,39 @@
 class Solution {
     public int countBinarySubstrings(String s) {
-                int count = 0;
-        int prevLength = 0;
-        int curLength = 1;
+       int zero = s.charAt(0)=='0'? 1:0;
+        int one = s.charAt(0)=='1'? 1:0;
+        int res =0;
+        for(int i =1 ; i< s.length();i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                if(s.charAt(i)=='0') zero++;
+                else one++;
 
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == s.charAt(i - 1)) {
-                curLength++; 
-            } else {
-                count += Math.min(prevLength, curLength); 
-                prevLength = curLength; 
-                curLength = 1; 
+            }else{
+                res+= Math.min(zero , one);
+                if(s.charAt(i)=='0')   zero=1;
+               else one =1;
             }
-        }
+     }
+     return res+Math.min(zero , one);
 
-        count += Math.min(prevLength, curLength); 
 
-        return count;
+        //         int count = 0;
+        // int prevLength = 0;
+        // int curLength = 1;
+
+        // for (int i = 1; i < s.length(); i++) {
+        //     if (s.charAt(i) == s.charAt(i - 1)) {
+        //         curLength++; 
+        //     } else {
+        //         count += Math.min(prevLength, curLength); 
+        //         prevLength = curLength; 
+        //         curLength = 1; 
+        //     }
+        // }
+
+        // count += Math.min(prevLength, curLength); 
+
+        // return count;
 
     }
 }
