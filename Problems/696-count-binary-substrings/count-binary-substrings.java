8c2bdb45e24df27fd476d7d02d70979728da254a
Prashantkumar Singh <43060569+Prashantsingh2508@users.noreply.github.com>
2024-02-13 17:27:26 +0000

    Time: 16 ms (12.83%) | Memory: 44.8 MB (81.44%) - LeetSync

--- Problems/696-count-binary-substrings/count-binary-substrings.java
+++ Problems/696-count-binary-substrings/count-binary-substrings.java
@@ -1,20 +1,45 @@
 class Solution {
     public int countBinarySubstrings(String s) {
-       int zero = s.charAt(0)=='0'? 1:0;
-        int one = s.charAt(0)=='1'? 1:0;
-        int res =0;
-        for(int i =1 ; i< s.length();i++){
-            if(s.charAt(i)==s.charAt(i-1)){
-                if(s.charAt(i)=='0') zero++;
-                else one++;
+       int c0=s.charAt(0)=='0'?1:0;
+        int c1=s.charAt(0)=='1'?1:0;
 
-            }else{
-                res+= Math.min(zero , one);
-                if(s.charAt(i)=='0')   zero=1;
-               else one =1;
+        int res=0;
+
+        for(int i=1;i<s.length();i++){
+
+            if(s.charAt(i)==s.charAt(i-1)){
+
+                    if(s.charAt(i)=='0'){
+
+                        c0++;
+                    }
+                    else{
+
+                        c1++;
+                    }
+
+
             }
-     }
-     return res+Math.min(zero , one);
+
+            else{
+                res=res+Math.min(c0,c1);
+                if(s.charAt(i)=='0'){
+
+                    c0=1;
+                }
+                else{
+
+                    c1=1;
+                }
+
+
+
+            }
+
+
+        }
+
+return res+Math.min(c0,c1);
 
 
         //         int count = 0;
