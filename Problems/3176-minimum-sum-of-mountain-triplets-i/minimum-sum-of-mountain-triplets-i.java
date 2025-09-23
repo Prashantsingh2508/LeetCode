16efdc087eb445782763dbf660762033578111d9
Prashantkumar Singh <43060569+Prashantsingh2508@users.noreply.github.com>
2025-09-23 21:01:33 +0100

    Time: 1 ms (100.00%) | Memory: 42.3 MB (27.64%) - LeetSync

---
+++ Problems/3176-minimum-sum-of-mountain-triplets-i/minimum-sum-of-mountain-triplets-i.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minimumSum(int[] arr) {
+         int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length - 2; i++) {
+
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = arr.length - 1;
+            while (left < right) {
+                if (arr[left] > arr[i]) {
+                    int sum1 = arr[left] + arr[i];
+                    while (right > left) {
+                        if (arr[right] < arr[left]) {
+                            min = Math.min(min, sum1 + arr[right]);
+                        }
+                        right--;
+                    }
+
+                }
+                right = arr.length - 1;
+                left++;
+
+//                if(  arr[right] < arr[left]) {
+//
+//                    min = Math.min(min, arr[i] + arr[left] + arr[right]);
+//
+//                }
+            }
+        }
+        
+        
+
+        return (min>100000)?-1:min;
+
+
+    }
+}
