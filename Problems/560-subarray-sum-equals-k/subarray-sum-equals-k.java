0b1337267db182f5b4f1ec9f00664f3444203c32
Prashantkumar Singh <43060569+Prashantsingh2508@users.noreply.github.com>
2025-09-27 17:07:47 +0100

    Time: 23 ms (92.80%) | Memory: 46.8 MB (33.75%) - LeetSync

--- Problems/560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ Problems/560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -2,24 +2,41 @@ class Solution {
     public int subarraySum(int[] arr, int k) {
         int n = arr.length;
         int count = 0;
-        int i = 0;
-        while (i < n) {
+        // int i = 0;
+        // while (i < n) {
 
-            if (arr[i] == k) {
-                count++;
+        //     if (arr[i] == k) {
+        //         count++;
 
+        //     }
+        //     int j = i + 1;
+        //     int temp = arr[i];
+        //     while (j < n) {
+        //         temp = temp + arr[j];
+        //         if (temp == k) {
+        //             count++;
+        //         }
+        //         j++;
+        //     }
+        //     i++;
+        // }
+ //int count = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int prefix = 0;
+
+        for (int a : arr) {
+            prefix += a;
+
+            if (map.containsKey(prefix - k)) {
+                count += map.get(prefix - k);
             }
-            int j = i + 1;
-            int temp = arr[i];
-            while (j < n) {
-                temp = temp + arr[j];
-                if (temp == k) {
-                    count++;
-                }
-                j++;
-            }
-            i++;
+
+            map.put(prefix, map.getOrDefault(prefix, 0) + 1);
+
         }
+
 
         return count;
 
