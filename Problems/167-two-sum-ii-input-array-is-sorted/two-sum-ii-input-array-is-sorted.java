853a050e4785c2c64bf24896f38a50dbbfe72931
Prashantkumar Singh <43060569+Prashantsingh2508@users.noreply.github.com>
2025-12-07 17:53:18 +0000

    Time: 7 ms (8.93%) | Memory: 49.2 MB (5.36%) - LeetSync

--- Problems/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ Problems/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,23 +1,42 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int left = 0;
-        int right = numbers.length - 1;
 
-        while (left < right) {
-            int currentSum = numbers[left] + numbers[right];
+   Map<Integer, Integer> map = new HashMap<>();
 
-            if (currentSum == target) {
-             
-                return new int[]{left + 1, right + 1};
-            } else if (currentSum < target) {
-              
-                left++;
-            } else {
-                
-                right--;
+        for (int i = 0; i < numbers.length; i++) {
+
+            int sum = target-numbers[i] ;
+            if (map.containsKey(sum)) {
+
+                return new int[]{map.get(sum) + 1, i + 1};
+
             }
+            map.put(numbers[i], i);
+
         }
 
-        return null;
+        return new int[]{};
+
+
+
+        // int left = 0;
+        // int right = numbers.length - 1;
+
+        // while (left < right) {
+        //     int currentSum = numbers[left] + numbers[right];
+
+        //     if (currentSum == target) {
+             
+        //         return new int[]{left + 1, right + 1};
+        //     } else if (currentSum < target) {
+              
+        //         left++;
+        //     } else {
+                
+        //         right--;
+        //     }
+        // }
+
+        // return null;
     }
 }
