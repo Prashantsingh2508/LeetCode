2e24a89ea9de7e1567936fc4eff44a7ee97f0bfc
Prashantkumar Singh <43060569+Prashantsingh2508@users.noreply.github.com>
2025-12-07 17:27:52 +0000

    Time: 2 ms (95.28%) | Memory: 48.2 MB (44.80%) - LeetSync

--- Problems/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ Problems/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -7,20 +7,17 @@ class Solution {
             int currentSum = numbers[left] + numbers[right];
 
             if (currentSum == target) {
-                // Found the solution.
-                // The problem requires 1-based indexing, so we add 1 to both indices.
+             
                 return new int[]{left + 1, right + 1};
             } else if (currentSum < target) {
-                // The sum is too small. Move the left pointer forward to increase the sum.
+              
                 left++;
             } else {
-                // The sum is too large. Move the right pointer backward to decrease the sum.
+                
                 right--;
             }
         }
 
-        // According to the problem constraints, this part should never be reached 
-        // because a solution is guaranteed to exist.
         return null;
     }
 }
